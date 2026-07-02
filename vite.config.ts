c953b24e4fbe252130037304342b9c6f9e70c6e8
Likith Reddy S <likithreddy@Mac-1.lan>
2026-07-03 02:54:05 +0530

    ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/streamV2', // Or whatever your repository name is
+  base: '/streamV2/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "./index.html"),
-      },
-    },
-  },
 })
